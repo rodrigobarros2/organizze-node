378b4396a0f7730dd571a2ed2609405543964a68
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-11-11 19:06:59 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const url = 'mongodb://localhost:27017/todo';
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = mongoose;
 
