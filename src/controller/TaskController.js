f4df6029a1692db3305b873717ca649622bb1564
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-11-11 19:06:43 -0300

    Update TaskController.js

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,14 +1,117 @@
+const {
+	startOfDay,
+	endOfDay,
+	startOfWeek,
+	endOfWeek,
+	startOfMonth,
+	endOfMonth,
+	startOfYear,
+	endOfYear,
+} = require('date-fns');
 const TaskModel = require('../model/TaskModel');
 
+const current = new Date();
+
 class TaskController {
-  /* vai criar uma nova tarefa no banco de dados */
-  // eslint-disable-next-line class-methods-use-this
-  async create(req, res) {
-    const task = new TaskModel(req.body);
-    await task
-      .save()
-      .then((response) => res.status(200).json(response))
-      .catch((error) => res.status(500).json(error));
-  }
+	async create(req, res) {
+		const task = new TaskModel(req.body);
+		await task
+			.save()
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async update(req, res) {
+		await TaskModel.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+			new: true,
+		})
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async all(req, res) {
+		await TaskModel.find({ macaddress: { $in: req.params.macaddress } })
+			.sort('when')
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async show(req, res) {
+		await TaskModel.findById(req.params.id)
+			.then((response) => {
+				if (response) {
+					return res.status(200).json(response);
+				}
+				return res.status(404).json({ error: 'tarefa não encontrada' });
+			})
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async delete(req, res) {
+		await TaskModel.deleteOne({ _id: req.params.id })
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async done(req, res) {
+		await TaskModel.findByIdAndUpdate(
+			{ _id: req.params.id },
+			{ done: req.params.done },
+			{ new: true }
+		)
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async late(req, res) {
+		await TaskModel.find({
+			when: { $lt: current },
+			macaddress: { $in: req.params.macaddress },
+		})
+			.sort('when')
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async today(req, res) {
+		await TaskModel.find({
+			macaddress: { $in: req.params.macaddress },
+			when: { $gte: startOfDay(current), $lte: endOfDay(current) },
+		})
+			.sort('when')
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async week(req, res) {
+		await TaskModel.find({
+			macaddress: { $in: req.params.macaddress },
+			when: { $gte: startOfWeek(current), $lte: endOfWeek(current) },
+		})
+			.sort('when')
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async month(req, res) {
+		await TaskModel.find({
+			macaddress: { $in: req.params.macaddress },
+			when: { $gte: startOfMonth(current), $lte: endOfMonth(current) },
+		})
+			.sort('when')
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
+
+	async year(req, res) {
+		await TaskModel.find({
+			macaddress: { $in: req.params.macaddress },
+			when: { $gte: startOfYear(current), $lte: endOfYear(current) },
+		})
+			.sort('when')
+			.then((response) => res.status(200).json(response))
+			.catch((error) => res.status(500).json(error));
+	}
 }
-module.export = new TaskController();
+
+module.exports = new TaskController();
