3396b9a2ef4da59cc9d2d12f865cffb8b80c65c6
Rodrigo Barros <rodrigobarros110@hotmail.com>
2020-11-11 19:08:31 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-  },
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	plugins: ['prettier'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'class-methods-use-this': 'off',
+	},
 };
